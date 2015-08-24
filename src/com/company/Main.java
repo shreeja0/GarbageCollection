bf3b89419944c4c61759323beffabc4e96e71145
shreeja <shreeja@plank.life>
2015-08-24 14:22:10 +0530

    Garbage Collection first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Runtime r = Runtime.getRuntime();
+        System.out.println("the free memory of JVM before garbage collection is " + r.freeMemory() );
+        r.gc();
+        System.out.println("the free memory after JVM after garbage collection is " + r.freeMemory());
+        
+    }
+}
